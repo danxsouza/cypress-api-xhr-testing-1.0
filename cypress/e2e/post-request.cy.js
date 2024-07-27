3a3b89465e20970aa49901e5ba15b08a7889fe92
Danilo Batista de Souza <danxsouza@proton.me>
2024-07-27 12:01:45 -0300

    Creating method GET and POST request API and validating

---
+++ cypress/e2e/post-request.cy.js
@@ -0,0 +1,15 @@
+describe('Post Request', () => {
+
+  it('Create a new post via /post api', () => {
+    cy.request({
+    method: 'POST',
+      url: 'http://localhost:3000/posts',
+      body: {
+          title: 'Want to learn automation testing?',
+           author: 'Danilo B de Souza'
+      }
+    }).then(response => {
+      expect(response.status).to.eq(201);
+    })
+  });
+})
